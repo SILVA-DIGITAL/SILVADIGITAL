1c19cfe8c80ff522a48a89aa5c3832e58d591327
Maximillian <maxsilvauk@gmail.com>
2021-02-02 23:36:46 +0000

    All external requirements stored in state to fix models not loading bug

--- src/components/Shapes/index.tsx
+++ src/components/Shapes/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useLayoutEffect, useEffect } from 'react'
+import React, { FC, useLayoutEffect, useState } from 'react'
 import * as THREE from 'three'
 import { a } from '@react-spring/three'
 import { Switch, Route } from 'wouter'
@@ -17,13 +17,11 @@ type GLTFResult = GLTF & {
 }
 
 const Shapes: FC = ({ transition }: any) => {
-  const torus = new THREE.TorusBufferGeometry(4, 1.2, 128, 128)
-  const torusknot = new THREE.TorusKnotBufferGeometry(3, 0.8, 256, 16)
+  const [torus] = useState(() => new THREE.TorusBufferGeometry(4, 1.2, 128, 128))
+  const [torusknot] = useState(() => new THREE.TorusKnotBufferGeometry(3, 0.8, 256, 16))
   const { nodes } = useGLTF('/assets/models/bomb-gp.glb') as GLTFResult
-
-  const material1 = new Material()
-  const material2 = new Material()
-  const material3 = new Material()
+  const [nodesstate] = useState(() => nodes)
+  const [materials] = useState<Array<Material>>([new Material(), new Material(), new Material()])
 
   const textures = useTexture([
     '/assets/images/ao.jpg',
@@ -41,15 +39,15 @@ const Shapes: FC = ({ transition }: any) => {
     <a.group {...props}>
       <Switch location={location}>
         <Route path="/">
-          <Shape geometry={torus} material={material1} textures={textures} color="white" opacity={opacity} />
+          <Shape geometry={torus} material={materials[0]} textures={textures} color="white" opacity={opacity} />
         </Route>
         <Route path="/knot">
-          <Shape geometry={torusknot} material={material2} textures={textures} color="#272730" opacity={opacity} />
+          <Shape geometry={torusknot} material={materials[1]} textures={textures} color="#272730" opacity={opacity} />
         </Route>
         <Route path="/bomb">
           <Shape
-            geometry={nodes.Little_Boy_Little_Boy_Material_0.geometry}
-            material={material3}
+            geometry={nodesstate.Little_Boy_Little_Boy_Material_0.geometry}
+            material={materials[2]}
             textures={textures}
             scale={[0.7, 0.7, 0.7]}
             rotation={[0, 0.5, 0]}
